ff12352b39f914448800f9f3bfc9e1e0f87fee43
PelusinniDev <110785576+pelusinnidev@users.noreply.github.com>
2024-11-08 16:41:46 +0100

    Starting the Project...
    - Hi han errors
    - No compila
    - Gradle dona problemes
    
    Així anem fent poc a poc en cas de que haguem de tornar enderrere...

--- app/src/main/java/com/example/androidstudio_koala_template/MainActivity.kt
+++ app/src/main/java/com/example/androidstudio_koala_template/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidstudio_koala_template.ui.theme.AndroidStudioKoalaTemplateTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidStudioKoalaTemplateTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Repte()
                 }
             }
         }
@@ -31,18 +42,86 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Repte() {
+    Column {
+        // Secció del Títol
+        Text(text = "Repte 01")
+
+        // Secció del Dropdown amb una colecció dels Icons.Default (tots o mínim 10)
+        DropDownMenu()
+
+        // Secció amb els Textfields de Mínim i Màxim (valors del Slider(min i max))
+        Textfields()
+
+        // Secció amb el Slider
+        Sliders()
+
+        // Dividim les Seccions amb un Divider
+        HorizontalDivider()
+
+        // Mostem el resultat (BadgedBox) amb: Icon(DropDownMenu) + Number(Slider + TextFields)
+        Result()
+    }
 }
 
-@Preview(showBackground = true
-    , showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun DropDownMenu(modifier: Modifier = Modifier) {
+    var selectedText: String by remember { mutableStateOf("") }
+    var expanded: Boolean by remember { mutableStateOf(false) }
+    val hobbies = listOf("Play music", "Practice sport", "Programming", "Reading", "Other")
+    Column(
+        Modifier
+            .padding(20.dp, 80.dp)
+    ) {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = { selectedText = it },
+            enabled = false,
+            readOnly = true,
+            modifier = Modifier
+                .clickable { expanded = true }
+                .fillMaxWidth()
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .fillMaxWidth()
+                // Afegir colors i detalls al desplegable:
+                .border(1.dp, Color.Black, RoundedCornerShape(4.dp))
+        ) {
+            hobbies.forEach { hobby ->
+                DropdownMenuItem(
+                    text = { Icons.Default },
+                    onClick = {
+                        expanded = false
+                        selectedText = hobby
+                    })
+            }
+        }
+    }
+}
+
+@Composable
+fun Textfields() {
+    Text(text = "Textfields")
+}
+
+@Composable
+fun Sliders() {
+    Text(text = "Textfields")
+}
+
+@Composable
+fun Result() {
+    Text(text = "Textfields")
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ReptePreview() {
     AndroidStudioKoalaTemplateTheme {
-        Greeting("Android")
+        Repte()
     }
 }
